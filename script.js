8b4a727880e6ceeffb2a472b7d5ef56fa9f7ae9b
maw1e <johnmarieygot21@gmail.com>
2024-03-23 17:22:28 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 "use-strict";
 
-let recipe = [];
+let recipes = [];
 
 const addBtn = document.querySelector("#addBtn");
 const inputName = document.querySelector("#name");
 const inputDesc = document.querySelector("#description");
 const inputInstructions = document.querySelector("#instructions");
 const inputIngredients = document.querySelector("#ingredients");
+let recipeContainerEl = document.querySelector(".recipe-container");
 
 // localStorage.clear();
 addBtn.addEventListener("click", () => {
@@ -21,12 +22,40 @@ addBtn.addEventListener("click", () => {
   newRecipe.desc = inputDesc.value;
   newRecipe.instructions = inputInstructions.value;
   newRecipe.ingredients = inputIngredients.value;
-  recipe.push(newRecipe);
-  localStorage.setItem("recipe", JSON.stringify(recipe));
-
+  recipes.push(newRecipe);
+  localStorage.setItem("recipe", JSON.stringify(recipes));
+  displayRecipe(recipes);
   inputDesc.value = "";
   inputName.value = "";
   inputIngredients.value = "";
+  inputInstructions.value = "";
 });
 
-const displayRecipe = () => {};
+const displayRecipe = (recipe) => {
+  let recipeList = "";
+  recipeContainerEl.innerHTML = "";
+
+  for (let i = 0; i < recipe.length; i++) {
+    recipeList += `<div class="recipe-wrapper">
+                  <h1>${recipe[i].name}</h1>
+                  <p>${recipe[i].desc}</p>
+                  <div class="buttons">
+                  <button id="preview">Preview</button>
+                  <button id="delete">Delete</button>
+                  </div>
+                  
+                  </div>`;
+  }
+  recipeContainerEl = document.querySelector(".recipe-container");
+
+  if (recipeContainerEl) {
+    recipeContainerEl.insertAdjacentHTML("beforeend", recipeList);
+  }
+};
+
+const recipeFromLocalStorage = JSON.parse(localStorage.getItem("recipe"));
+
+if (recipeFromLocalStorage) {
+  recipes = recipeFromLocalStorage;
+  displayRecipe(recipes);
+}
